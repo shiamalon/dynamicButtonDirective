53a6ff19c8712b4fed180a15c9e613ed2bc0ddd3
Joao Reis <jreis@uptype.it>
2018-03-22 15:51:24 +0000

    add buttons as fuck

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DivComponentComponent } from './div-component/div-component.component';
 import { ButtonDirectiveDirective } from './button-directive.directive';
+import { ButtonComponent } from './button/button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DivComponentComponent,
-    ButtonDirectiveDirective
+    ButtonDirectiveDirective,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ButtonComponent]
 })
 export class AppModule { }
--- src/app/button-directive.directive.ts
+++ src/app/button-directive.directive.ts
@@ -1,10 +1,39 @@
-import { Directive } from '@angular/core';
+import {
+  Directive, ElementRef, HostListener, Input, TemplateRef, ViewContainerRef,
+  ComponentRef, ComponentFactoryResolver, Renderer, AfterViewInit
+} from '@angular/core';
+import { ButtonComponent } from './button/button.component';
 
 @Directive({
-  selector: '[appButtonDirective]'
+  selector: '[appButtonDirective]',
 })
 export class ButtonDirectiveDirective {
 
-  constructor() { }
+  private button: ComponentRef<ButtonComponent>;
+
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private resolver: ComponentFactoryResolver,
+    private elRef: ElementRef,
+    private renderer: Renderer
+  ) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.renderer.setElementClass(this.elRef.nativeElement, 'ps-tooltip-trigger', true); // ok
+
+    // factory comp resolver
+    const factory = this.resolver.resolveComponentFactory(ButtonComponent);
+
+    // create component
+    this.button = this.viewContainerRef.createComponent(factory);
+    console.log(this.button);
+
+    // set content of the component
+    this.elRef.nativeElement.appendChild(this.button.location.nativeElement);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.elRef.nativeElement.style.backgroundColor = null;
+  }
 
 }
